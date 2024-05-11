88b141551590bcf75f53435506f9ff18cf1d525c
Jorge Palacio <jorgepalacio1995@gmail.com>
2024-05-10 20:20:39 -0500

    avance

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ enum CarType {
 
 type Color = 'red' | 'blue' | 'green';
 
+
+
 interface CreateCar {
   color: Color;
   year: number;
@@ -28,6 +30,7 @@ interface ICar {
 
 
 class Car implements ICar {
+  private name: String;
   private color: Color;
   private year: number;
   private type: CarType;
@@ -38,6 +41,24 @@ class Car implements ICar {
     this.year = year;
     this.type = type;
     this.brand = brand;
+    switch (type) {
+      case CarType.SEDAN:
+        this.name = this.brand + " - Sedan - " + this.color + " - " + year
+        break;
+      case CarType.SUV:
+        this.name = this.brand + " - Suv - " + this.color + " - " + year
+        break;
+      case CarType.TRUCK:
+        this.name = this.brand + " - Truck - " + this.color + " - " + year
+        break;
+      case CarType.VAN:
+        this.name = this.brand + " - Van - " + this.color + " - " + year
+        break;
+    }
+  }
+
+  get getName(): String {
+    return this.name
   }
 
   get getColor (): Color {
@@ -71,15 +92,32 @@ class Car implements ICar {
 }
 
 let car = new Car({brand: 'Toyota', color: 'red', type: CarType.SEDAN, year: 2020});
-console.log("============= Car =============");
-console.log("brand: ", car.getBrand());
-console.log("color: ", car.getColor);
-console.log("typeof car: ", typeof car);
-console.log("typeof carType: ", typeof car.getType);
+// console.log("============= Car =============");
+// console.log("brand: ", car.getBrand());
+// console.log("color: ", car.getColor);
+// console.log("typeof car: ", typeof car);
+// console.log("typeof carType: ", typeof car.getType);
 
 let car2 = new Car({brand: 'Nissan', color: 'green', type: CarType.SUV, year: 2019});
-console.log("============= Car 2 =============");
-console.log("brand: ", car2.getBrand());
-console.log("color: ", car2.getColor);
-console.log("typeof car: ", typeof car2);
-console.log("typeof carType: ", typeof car2.setBrand);
+// console.log("============= Car 2 =============");
+// console.log("brand: ", car2.getBrand());
+// console.log("color: ", car2.getColor);
+// console.log("typeof car: ", typeof car2);
+// console.log("typeof carType: ", typeof car2.setBrand);
+
+let car3 = new Car({brand: 'Nissan', color: 'blue', type: CarType.SUV, year: 2019});
+// console.log("============= Car 3 =============");
+// console.log("brand: ", car3.getBrand());
+// console.log("color: ", car3.getColor);
+// console.log("typeof car: ", typeof car3);
+// console.log("typeof carType: ", typeof car3.setBrand);
+
+type PropertiesCar = keyof Car;
+
+let cars = [car, car2]
+
+function filterCar(property: PropertiesCar, value: Car[PropertiesCar]){
+  return cars.filter((car)=> car[property] == value);
+}
+
+console.log(filterCar("getColor", "blue"))
